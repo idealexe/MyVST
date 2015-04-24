74345979b1c8489ab107c94b49285737ea2c1dd1
idealexe <omega_8810@yahoo.co.jp>
2015-04-25 01:16:01 +0900

    Revert "CoreTweet"
    This reverts commit 80e7e067bb05b25a22c1fa446bcc89ba58a0f301.

--- CoreTweet/CoreTweet.Shared/AssemblyVersion.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("0.4.2.*")]
-[assembly: AssemblyInformationalVersion("0.4.2")]
